0115a546ac35e48b72f0b495342d959745132346
luxhm <lux.hogan-murphy22@trinityschoolnyc.org>
2022-02-28 20:13:47 -0500

    Working on controllers

--- controllers/manifest_controller.js
+++ controllers/manifest_controller.js
@@ -11,7 +11,7 @@ router.get('/loadManifestations', function(request, response){
 
       response.status(200);
       response.setHeader('Content-Type', 'text/html')
-      response.render("loadManifestations",{
+      response.render("manifest/loadManifestations",{
         name: name,
         manifest: manifest
   });
@@ -19,16 +19,14 @@ router.get('/loadManifestations', function(request, response){
   else {
     response.status(400);
     response.setHeader('Content-Type', 'text/html')
-    response.render("error", {
-      "errorCode":"400"
-    });
+    response.redirect('/error?code=404'););
   }
 });
 
 router.get('/manifest', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
-  response.render("manifest");
+  response.render("manifest/manifest");
 });
 
 module.exports = router
