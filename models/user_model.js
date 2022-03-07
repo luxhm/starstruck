3a2d9179d593b1ee4ff33581053260386e80bc90
nathalieperra22 <nathalie.perra22@trinityschoolnyc.org>
2022-03-07 11:32:55 -0500

    Adding api !! not working yet lol

--- models/user_model.js
+++ models/user_model.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+let axios = require('axios'); //install with npm install axios
 
 exports.getUser = function(){
   let users = JSON.parse(fs.readFileSync(__dirname+'/../data/users.json'));
@@ -33,4 +34,14 @@ exports.saveAstrology = function(userID, userName, birthdate, birthplace, birtht
   }
 
   console.log("astrology saved");
+}
+
+//API Async router
+exports.getInfo = async function(title){
+  try {
+       const resp = await axios.get('https://json.astrologyapi.com/v1/western_horoscope'+api);
+       return resp.data;
+   } catch (err) {
+       console.error(err);
+   }//the try catch is necessary
 }
