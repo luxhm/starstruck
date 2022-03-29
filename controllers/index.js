157df81223eba2cb6bf8e29723c2f721651d2a05
luxhm <lux.hogan-murphy22@trinityschoolnyc.org>
2022-03-29 14:14:34 -0400

    Working on logged in capabilities

--- controllers/index.js
+++ controllers/index.js
@@ -4,13 +4,17 @@ const express = require('express'),
 router.get('/', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
-  response.render("login");
+  response.render("login", {
+    user: request.user
+  });
 });
 
 router.get('/login', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'text/html')
-  response.render("login");
+  response.render("login", {
+    user: request.user
+  });
 });
 
 router.get('/error', function(request, response) {
