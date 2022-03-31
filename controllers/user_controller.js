779e8323b5a29f9930a6324308108d8091e457c5
luxhm <lux.hogan-murphy22@trinityschoolnyc.org>
2022-03-30 23:29:20 -0400

    Update user_controller.js

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -38,8 +38,8 @@ router.get('/astrologyInfo', function(request, response) {
 router.get('/horoscope', async function(request, response) {
 
   let userID = request.user._json.email;
-  let userInfo = User.getUser;
-  let sign = userInfo[userID.sunSign];
+  let userInfo = User.getUser();
+  let sign = userInfo[userID].sunSign;
   let data = await User.getHoroscope(sign);
   response.send(data);
   response.status(200);
